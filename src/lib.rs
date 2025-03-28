499152268e99d8d794104186e6fa810b0863a49b
hackia <hackia@eytukan.com>
2025-03-28 17:59:18 +0100

    remove unimplemented modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,5 @@
 #[doc = "Testing frameworks"]
 pub mod anima;
-pub mod build;
-pub mod config;
-pub mod exec;
-pub mod graph;
-pub mod image;
-pub mod layer;
-pub mod plan;
+
+#[doc = "All utilities"]
 pub mod utils;
